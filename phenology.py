8c4df808c195a75aab117a66fd5fca8ec1b03192
heisenberg <heisenberg0711@gmail.com>
2019-02-28 15:44:12 -0600

    s

--- phenology.py
+++ phenology.py
@@ -1,14 +1,23 @@
 import numpy as np
+import pandas as pd
+import geopandas as gpd
 import rasterio
+import rasterio.mask
 from matplotlib import pyplot as plt
+%matplotlib inline
 
-CDL pixel size: [0.000325888636548,-0.000325899193786]
-    image size: [12329, 16993]
+# CDL pixel size: [0.000325888636548,-0.000325899193786]
+#     image size: [12329, 16993]
+#
+# GCVI pixel size: [0.005418574049116,-0.005418574182687]
+#     image size: [742, 1023]
 
-GCVI pixel size: [0.005418574049116,-0.005418574182687]
-    image size: [742, 1023]
+df = pd.read_csv('maize.alldat.nolatlon.csv')
 
-
+IL = df['state']=='IL'
+IL_county = df[IL]
+col_list = ['FIPS','sday']
+IL_county = IL_county[col_list]
 
 with rasterio.open('transformed.tif') as src:
     transformed_array = src.read(1)
